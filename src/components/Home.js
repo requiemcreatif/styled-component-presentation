c1f8effcab900dffb91b8ba9d1f3e806f0d42419
OmerutaPro <omeruta@OmerutaPros-MacBook-Pro.local>
2022-11-01 15:58:53 +0100

    Last version of Styled Component

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = ({images}) => {
 /*MAIN CONTAINER*/
     const MainContainer = styled.div`
         margin: 0 auto;
-        max-width: 1200px;
+        max-width: 1300px;
         box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
 
     `
@@ -27,7 +27,7 @@ const Home = ({images}) => {
         text-decoration: none;
         color: white;
     `
-
+    /*STYLING HEADER*/
     const Heading = styled.h1`
         text-align: center;
         font-size: 3rem;
@@ -44,25 +44,42 @@ const Home = ({images}) => {
     `
     /*TOP PART*/
     const TopPart = styled.section`
-        //background-color: #A1C7E0;
+        padding: 3rem;
+    `
+
+    /* TEXT PART*/
+    const TextPart = styled.div`
+        background-color: #F2F2F2;
+        border-radius: 10px;
         padding: 2rem;
     `
 
     /*IMAGE CONTAINER */
-
     const ImageSection = styled.section`
         display: grid;
         grid-template-columns: 1fr 1fr;
+        grid-gap: 2rem;
+        padding: 3rem;
+        background-color: #F2F2F2;
+        // 1 column
+        @media (max-width: 768px) {
+            grid-template-columns: 1fr;
+        }
     `
     const ImageContainer = styled.div`
-        padding: 2rem;
         display: flex;
         flex-direction: column;
-        gap: 1rem;
+        border-radius: 10px;
     `
     const Image = styled.img`
         width: 100%;
-        border-radius: 15px;
+        border-radius: 10px 10px 0 0;
+    `
+
+    const ImageText = styled.div`
+        padding: 1rem; 
+        border-radius: 0 0 10px 10px;
+        border: 1px solid #ccc;
     `
     /*FOOTER*/
     const Footer = styled.footer`
@@ -90,10 +107,10 @@ const Home = ({images}) => {
             <TopPart>
                     <Heading>Styled Components</Heading>
                     <SmallHeading>This is what you can do with Styled Component</SmallHeading>
-                <div>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo totam cupiditate voluptatum. Reiciendis autem consequatur iusto suscipit expedita amet exercitationem, inventore, possimus nesciunt corporis aperiam quibusdam facere? Saepe, nam?</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo totam cupiditate voluptatum. Reiciendis autem consequatur iusto suscipit expedita amet exercitationem, inventore, possimus nesciunt corporis aperiam quibusdam facere? Saepe, nam?</p>
-                </div>
+                <TextPart>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo totam cupiditate voluptatum. Reiciendis autem consequatur iusto suscipit expedita amet exercitationem, inventore, possimus nesciunt corporis aperiam quibusdam facere? Saepe, nam?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi nemo totam cupiditate voluptatum. Reiciendis autem consequatur iusto suscipit expedita amet exercitationem, inventore, possimus nesciunt corporis aperiam quibusdam facere? Saepe, nam?</p>
+                </TextPart>
                 
             </TopPart>
             {/* PART 3 */}
@@ -101,9 +118,12 @@ const Home = ({images}) => {
             <ImageSection>  
                 {images.map((image) => (
                     <ImageContainer>
-                        <h2>{image.title} </h2>
+                        
                         <Image src={image.imgUrl} alt="" />
+                        <ImageText>
+                        <h2>{image.title} </h2>
                         <p>{image.text} </p>
+                        </ImageText>
                     </ImageContainer>
                 ))}  
             </ImageSection>
